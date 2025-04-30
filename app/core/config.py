8900e30fdbc9a0531ca757ec4b0870d69111b4a4
Manisha Bhandari <manishabhandari841@gmail.com>
2025-04-30 22:20:50 +0545

    refactor: remove unnecessary comments

--- app/core/config.py
+++ app/core/config.py
@@ -1,35 +1,3 @@
-# from pydantic import BaseSettings
-
-
-# class Settings(BaseSettings):
-#     PROJECT_NAME: str = "FastAPI Auth0 M2M Demo"
-#     API_V1_STR: str = "/api/v1"
-
-#     AUTH0_DOMAIN: str = "dev-g1fkrxywfwaxnmmg.us.auth0.com"
-#     AUTH0_API_AUDIENCE: str = "https://dev-g1fkrxywfwaxnmmg.us.auth0.com/api/v2/"
-#     AUTH0_ALGORITHMS: list = ["RS256"]
-#     JWKS_URL: str = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
-#     SECRET_KEY: str = "123secrtekey123"
-#     ALGORITHM: str = "HS256"
-#     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
-#     M2M_APPLICATIONS: dict = {
-#         "app1": {
-#             "app_id": "app1",
-#             "client_id": "UiCirtVTsAhNUlD1IgUhxBGHx7JYPYr8",
-#             "client_secret": "jLsrBtvTXVucscwhN0oC9JdXXu3Zt-BXHk9E2zRzSMt6LaK9_9VOgy8pufNt2dcF",
-#             "grant_type": "client_credentials",
-#         }
-#     }
-
-#     class Config:
-#         case_sensitive = True
-#         env_file = ".env"
-
-
-# settings = Settings()
-
-
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from typing import Dict, Any
